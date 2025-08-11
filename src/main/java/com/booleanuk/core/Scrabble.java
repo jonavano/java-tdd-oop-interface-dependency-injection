3d4ab17f6eab65ce554bd4275da8843599a45294
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-08-11 11:11:53 +0200

    implemented all alphabets

--- src/main/java/com/booleanuk/core/Scrabble.java
+++ src/main/java/com/booleanuk/core/Scrabble.java
@@ -5,9 +5,8 @@ import java.util.Map;
 public class Scrabble {
     Map<Character, Integer> letterScores;
 
-    public Scrabble() {
-        Alphabet a = new Alphabet();
-        this.letterScores = a.getLetterScores();
+    public Scrabble(Alphabet alphabet) {
+        this.letterScores = alphabet.getLetterScores();
     }
 
     public int score(String word) {
